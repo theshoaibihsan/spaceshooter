8db9f7d35adcc31456ed6251b49298f255eed594
The shoaib Ihsan <theshoaibihsan9@gmail.com>
2020-01-18 15:09:06 +0500

    space jet is created

---
+++ Player_Movement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour {
+    [SerializeField]
+   private float speed = 5f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Movement();
+    }
+
+
+
+
+    private void Movement()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.right * speed * horizontal * Time.deltaTime);
+        transform.Translate(Vector3.up * speed * vertical * Time.deltaTime);
+        if (transform.position.y > 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        }
+        else if (transform.position.y < -3.6f)
+        {
+            transform.position = new Vector3(transform.position.x, -3.6f, 0);
+
+
+        }
+        if (transform.position.x > 8)
+        {
+            transform.position = new Vector3(8, transform.position.y, 0);
+        }
+        else if (transform.position.x < -8)
+        {
+            transform.position = new Vector3(-8, transform.position.y, 0);
+        }
+    }
+   
+}
