c1539584fb08032d6caa8f2274cca7a7391a43f7
The shoaib Ihsan <theshoaibihsan9@gmail.com>
2020-01-20 13:19:18 +0500

    spawn

---
+++ Spawn_Manger.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn_Manger : MonoBehaviour
+{
+    public GameObject enemy_ship;
+    public GameObject[] extra;
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(enemy_spawn_routine());
+        StartCoroutine(speed());
+        StartCoroutine(shield());
+        StartCoroutine(tripleshot());
+    }
+
+    // Update is called once per frame
+    IEnumerator enemy_spawn_routine()
+    {
+        while (true)
+        {
+
+            Instantiate(enemy_ship, new Vector3(Random.Range(-7, 7), 7), Quaternion.identity);
+            yield return new WaitForSeconds(3.0f);
+        }
+    }
+
+
+
+    //speed up spawn
+    IEnumerator speed()
+    {
+        while (true)
+        {
+
+            Instantiate(extra[1], new Vector3(Random.Range(-8, 7), 7), Quaternion.identity);
+            yield return new WaitForSeconds(20.0f);
+        }
+    }
+    //shield
+    IEnumerator shield()
+    {
+        while (true)
+        {
+
+            Instantiate(extra[0], new Vector3(Random.Range(-6, 7), 7), Quaternion.identity);
+            yield return new WaitForSeconds(9.0f);
+        }
+    }
+    //tripleshot
+    IEnumerator tripleshot()
+    {
+        while (true)
+        {
+
+            Instantiate(extra[2], new Vector3(Random.Range(-6, 7), 7), Quaternion.identity);
+            yield return new WaitForSeconds(15.0f);
+        }
+    }
+}
